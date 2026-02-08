74d0c7b9725280fe1e2de5ae9dae146fb72eac4e
dhruv bishnoi <bishnoishab502@gmail.com>
2026-02-08 20:43:48 +0530

    Create areaS.java

---
+++ function/areaS.java
@@ -0,0 +1,13 @@
+package function;
+import java.util.*;
+public class areaS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int length = sc.nextInt();
+
+        int area = length*length;
+        System.out.println(area);
+        
+    }
+}
